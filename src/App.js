9e4b59832c499e83250c90446f821a590cdd2b85
AlexisEstebanJaureguiCoraite <153698796+AlexisEstebanJaureguiCoraite@users.noreply.github.com>
2023-12-13 01:35:53 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Componente1 } from './componentes/Componente1';
+import { Componente2 } from './componentes/Componente2';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Componente1></Componente1>
+        <Componente2></Componente2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
